6a1b476bbecbad72a1d395dd8d4d0f1af6e9db64
Jaishnav <jaishnavgopi@gmail.com>
2020-12-01 15:49:39 +0530

    Navbar corrections

--- src/components/header.js
+++ src/components/header.js
@@ -9,40 +9,40 @@ const Header = (props) => {
 	return (
 		<div>
 			<Navbar expand="md" fixed="top">
-				<NavbarToggler onClick={toggle}></NavbarToggler>
 				<Collapse isOpen={isOpen} navbar className="order-md-0">
 					<Nav className="ml-auto" navbar>
-	          <NavItem className="mx-auto">
-	            <a href="/" className="nav-link">HOME</a>
+	          <NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">HOME</a>
 	          </NavItem>
-	          <NavItem className="mx-auto">
-	            <a href="/" className="nav-link">ABOUT</a>
+	          <NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">ABOUT</a>
 	          </NavItem>
-	          <NavItem className="mx-auto">
-	            <a href="/" className="nav-link">WORK</a>
+	          <NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">WORK</a>
 	          </NavItem>
-	        	<NavItem className="mx-auto">
-	            <a href="/" className="nav-link">COMPETITIONS</a>
+	        	<NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">COMPETITIONS</a>
 	          </NavItem>
 	        </Nav>
 				</Collapse>
-				<a href="/" className="navbar-brand order-md-1 mx-auto"><img id="logo" src="./assets/images/robologo.png" style={{height: 50}}></img></a>
+				<a href="/" className="navbar-brand order-md-1 mx-auto" style={{paddingRight: '1rem'}}><img id="logo" src="./assets/images/logo2.jpg" style={{height: 50}}></img></a>
 				<Collapse isOpen={isOpen} navbar className="order-md-2">
 					<Nav className="mr-auto" navbar>
-	          <NavItem className="mx-auto">
-	            <a href="/" className="nav-link">TEAM</a>
+	          <NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">TEAM</a>
 	          </NavItem>
-	          <NavItem className="mx-auto">
-	            <a href="/" className="nav-link">GALLERY</a>
+	          <NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">GALLERY</a>
 	          </NavItem>
-	          <NavItem className="mx-auto">
-	            <a href="/" className="nav-link">SPONSORS</a>
+	          <NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">SPONSORS</a>
 	          </NavItem>
-	        	<NavItem className="mx-auto">
-	            <a href="/" className="nav-link">CONTACT</a>
+	        	<NavItem className="mx-auto navlink">
+	            <a href="/" className="navlink nav-link">CONTACT</a>
 	          </NavItem>
 	        </Nav>
 				</Collapse>
+				<NavbarToggler onClick={toggle}></NavbarToggler>
 			</Navbar>
 		</div>
 	);
